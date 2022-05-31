5e8f58b280d73da70d1f908215e55841538fa5ae
ojosdegatonegro <diegofernandocubides@gmail.com>
2022-05-31 18:11:12 -0500

    adds rounds

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -12,49 +12,77 @@ function computerPlay() {
   }
 }
 
-function playRound(computerSelection) {
-  let playerSelection = prompt('Make your choice:');
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection === 'rock') {
-    switch (computerSelection) {
-      case 'paper':
-        return `Your choice: ${playerSelection}, computer's choice: ${computerSelection}. Computer wins!!!`;
-        break;
-      case 'scissors':
-        return `Your choice: ${playerSelection}, computer's choice: ${computerSelection}. You win!!!`;
-        break;
-      default:
-        return `It's a tie`;
-    }
-  } else if (playerSelection === 'paper') {
-    switch (computerSelection) {
-      case 'scissors':
-        return `Your choice: ${playerSelection}, computer's choice: ${computerSelection}. Computer wins!!!`;
-        break;
-      case 'rock':
-        return `Your choice: ${playerSelection}, computer's choice: ${computerSelection}. You win!!!`;
-        break;
-      default:
-        return `It's a tie`;
-    }
-  } else {
-    switch (computerSelection) {
-      case 'rock':
-        return `Your choice: ${playerSelection}, computer's choice: ${computerSelection}. Computer wins!!!`;
-        break;
-      case 'paper':
-        return `Your choice: ${playerSelection}, computer's choice: ${computerSelection}. You win!!!`;
-        break;
-      default:
-        return `It's a tie`;
-    }
-  }
-}
-
-function game() {
+function playRound() {
+  let userWins = 0;
+  let PCWins = 0;
+  let ties = 0;
   for (let i = 1; i <= 5; i++) {
-    console.log(playRound(computerPlay()));
+    let computerSelection = computerPlay();
+    let playerSelection = prompt('Make your choice:');
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === 'rock') {
+      switch (computerSelection) {
+        case 'paper':
+          PCWins = PCWins + 1;
+          console.log(
+            `Your choice: ${playerSelection}, computer's choice: ${computerSelection}. Computer wins!!!`
+          );
+          break;
+        case 'scissors':
+          userWins = userWins + 1;
+          console.log(
+            `Your choice: ${playerSelection}, computer's choice: ${computerSelection}. You win!!!`
+          );
+          break;
+        default:
+          ties = ties + 1;
+          console.log(`It's a tie`);
+      }
+    } else if (playerSelection === 'paper') {
+      switch (computerSelection) {
+        case 'scissors':
+          PCWins = PCWins + 1;
+          console.log(
+            `Your choice: ${playerSelection}, computer's choice: ${computerSelection}. Computer wins!!!`
+          );
+          break;
+        case 'rock':
+          userWins = userWins + 1;
+          console.log(
+            `Your choice: ${playerSelection}, computer's choice: ${computerSelection}. You win!!!`
+          );
+          break;
+        default:
+          ties = ties + 1;
+          console.log(`It's a tie`);
+      }
+    } else {
+      switch (computerSelection) {
+        case 'rock':
+          PCWins = PCWins + 1;
+          console.log(
+            `Your choice: ${playerSelection}, computer's choice: ${computerSelection}. Computer wins!!!`
+          );
+          break;
+        case 'paper':
+          userWins = userWins + 1;
+          console.log(
+            `Your choice: ${playerSelection}, computer's choice: ${computerSelection}. You win!!!`
+          );
+          break;
+        default:
+          ties = ties + 1;
+          console.log(`It's a tie`);
+      }
+    }
+  }
+  if (userWins > PCWins) {
+    console.log(`You win, ${userWins} to ${PCWins}`);
+  } else if (userWins < PCWins) {
+    console.log(`You lose, ${userWins} to ${PCWins}`);
+  } else {
+    console.log(`It's a tie`);
   }
 }
 
-game();
+playRound();
